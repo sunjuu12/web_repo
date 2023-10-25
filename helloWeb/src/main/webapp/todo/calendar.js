8368aec37b32de4663fcee06bad24cccf1480e75
haeju <haeju@DESKTOP-EGQH0TO>
2023-10-25 23:14:43 +0900

    ....

--- helloWeb/src/main/webapp/todo/calendar.js
+++ helloWeb/src/main/webapp/todo/calendar.js
@@ -3,41 +3,50 @@
 // makeHead()
 
 const monAry = ['일',' 월', '화', '수', '목', '금', '토'];
-const days = new Date();
+const daysAry = [1, 2, 3, ... 31];
 console.log(monAry);
 console.log(days);
 
-class calendar {
-	makeHead(monAry) {
-	let head = '';
-	head += `<table border = "1"><thead>`;
-	head += `<h3>`+ '10월' +`</h3>`;
-	head += `<tr>`;
-	for ( let mon in monAry) {
-		head += `<th>`+monAry[mon] + '</th>';
-	}
-	
-	head += `</tr></thead></table>`;
-	return tr;
-	} // end makeHead
-	
+
+
+class Calendar {
+  makeHead() {
+    let head = '';
+    head += `<table border="1">`;
+    head += `<caption>10월</caption>`;
+    head += `<thead><tr>`;
+    for (let day of monAry) {
+      head += `<th>${day}</th>`;
+    }
+    head += `</tr></thead></table>`;
+    return head;
+  }
+
 	// makeBody()
 	makeBody() {
-		let tr = '';
-		for (let i = 1; i <= days; i++) {
-			if(i % 7 == 0) {
-				tr += `<tr>`;
-				tr += `<td>`+ days +`</td>`;
-				tr += `</tr>`;
-			}
-		} 
+		for (let i = 0; i < daysAry.length; i++) {
+	        if (i % 7 == 0) {
+	            tr += '<tr>'; 
+        	}
+        		for(let j = 0; j = 6; j++) {
+					tr += `<td>${datesArray[i]}</td>`;
+				}
+			        if (i == daysAry.length - 1) {
+			            tr += '</tr>'; 
+			        }
+    	}
+    	return tr;
 	}// end makeBody
 	
 	// makeCalendar();
 	makeCalendar() {
-		
-	}
-	// end makeCalendar();
+	    const head = this.makeHead();
+	    const body = this.makeBody();
+
+    return `<table>${head}${body}</table>`;
+  } // end makeCalendar();
+	
 	}
 	
-calendar.makeHead(monAry);
+Calendar.makeHead(monAry);
+document.getElementById('calendarContainer');
